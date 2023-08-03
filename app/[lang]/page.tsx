e0e4505674f376a310898be45013fcf6043e2b81
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-03 11:39:23 +1000

    type fixing

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,11 +1,12 @@
 import { AppSelect } from '@/app/[lang]/_components/app-select'
 import { useLocale } from 'next-intl'
+import { Locale } from '@/i18n-config'
 
 export default function Home() {
   const locale = useLocale()
   return (
     <main className="flex justify-center items-center w-screen h-screen">
-      <AppSelect name="bill-splitting" locale={locale} />
+      <AppSelect name="bill-splitting" locale={locale as Locale} />
     </main>
   )
 }
