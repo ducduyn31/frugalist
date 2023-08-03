be1490b111e829f04e94f7dbde83335ebbed0180
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-03 11:24:37 +1000

    modal slot should not be optional

--- app/[lang]/bill-splitting/bills/layout.tsx
+++ app/[lang]/bill-splitting/bills/layout.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react'
 
 interface Props extends PropsWithChildren<{}> {
-  modal?: React.ReactNode
+  modal: React.ReactNode
 }
 
 export default async function BillsLayout({ children, modal }: Props) {
