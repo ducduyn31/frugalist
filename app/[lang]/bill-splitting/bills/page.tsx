76f774b16604b63a7aafbc5cbb9401b306685604
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-03 13:01:37 +1000

    Replace PageHeader with shared component

--- app/[lang]/bill-splitting/bills/page.tsx
+++ app/[lang]/bill-splitting/bills/page.tsx
@@ -1,31 +1,11 @@
 import React from 'react'
 import { NoBillState } from '@/app/[lang]/bill-splitting/bills/_components/no-bill-state'
-import Link from 'next/link'
-import { getTranslator } from 'next-intl/server'
+import { PageHeader } from '@/components/page-header'
 
-interface Props {
-  params: { lang: string }
-}
-
-export default async function AddBills({ params: { lang } }: Props) {
-  const t = await getTranslator(lang, 'common.PageHeader.bill-splitting.bills')
+export default function AddBills() {
   return (
     <>
-      <header className="flex justify-between">
-        <div>
-          <h1 className="text-3xl font-semibold text-base-content">
-            {t('title')}
-          </h1>
-          <p className="text-base-content/50">{t('subtitle')}</p>
-        </div>
-        <Link
-          prefetch
-          href="bills/new"
-          className="btn btn-primary btn-md px-8 self-end"
-        >
-          {t('button')}
-        </Link>
-      </header>
+      <PageHeader namespace="bill-splitting.bills" addAction="people/new" />
       <main className="mt-20">
         <NoBillState />
       </main>
--- app/[lang]/bill-splitting/people/page.tsx
+++ app/[lang]/bill-splitting/people/page.tsx
@@ -1,31 +1,11 @@
 import React from 'react'
 import { NoPersonState } from '@/app/[lang]/bill-splitting/people/no-person-state'
-import Link from 'next/link'
-import { getTranslator } from 'next-intl/server'
+import { PageHeader } from '@/components/page-header'
 
-interface Props {
-  params: { lang: string }
-}
-
-export default async function PeopleEdit({ params: { lang } }: Props) {
-  const t = await getTranslator(lang, 'common.PageHeader.bill-splitting.people')
+export default function PeopleEdit() {
   return (
     <>
-      <header className="flex justify-between">
-        <div>
-          <h1 className="text-3xl font-semibold text-base-content">
-            {t('title')}
-          </h1>
-          <p className="text-base-content/50">{t('subtitle')}</p>
-        </div>
-        <Link
-          prefetch
-          href="people/new"
-          className="btn btn-primary btn-md px-8 self-end"
-        >
-          {t('button')}
-        </Link>
-      </header>
+      <PageHeader namespace="bill-splitting.people" addAction="people/new" />
       <main className="mt-20">
         <NoPersonState />
       </main>
