a73f327de6a4f37236a52edca101f44a5b25adb3
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-06 19:05:38 +1000

    add event form

--- app/[lang]/bill-splitting/calendar/event-calendar.tsx
+++ app/[lang]/bill-splitting/calendar/event-calendar.tsx
@@ -12,8 +12,6 @@ interface Props {
 export const EventCalendar: React.FC<Props> = ({ initialEvents }) => {
   const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 })
 
-  console.log(initialEvents)
-
   return (
     <div className="h-[70vh] w-full">
       <Calendar localizer={localizer} events={initialEvents} />
